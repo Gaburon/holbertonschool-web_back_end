ff741a7c27238d7ee4faff64be7af07a998ccb30
gaburon <japo000@hotmail.com>
2023-09-01 10:54:56 -0500

    Fixing readme

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-"""Async Tasks"""
+""" typing and asyncio """
 import asyncio
 from typing import List
 
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Returns a list of delayed floats by using tasks"""
-    tasks = [task_wait_random(max_delay) for i in range(n)]
-    return [await t for t in asyncio.as_completed(tasks)]
+    """Returns list of delayed tasks"""
+    results: List[float] = [task_wait_random(
+        max_delay) for _ in range(n)]
+    return [await result for result in asyncio.as_completed(results)]
